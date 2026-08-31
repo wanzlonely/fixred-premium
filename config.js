05c3c92ec0c149e0649bad0df238537bceba0d6d
wanzlonely <wanzlonely04@gmail.com>
2026-09-01 00:27:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-module.exports = {
+module.exports={
 BOT_TOKEN: process.env.BOT_TOKEN ? process.env.BOT_TOKEN.replace(/['"]/g, '').trim() : '',
 API_ID: Number(process.env.API_ID),
 API_HASH: process.env.API_HASH ? process.env.API_HASH.replace(/['"]/g, '').trim() : '',
@@ -9,9 +9,9 @@ TARGET_BOTS: process.env.TARGET_BOTS ? process.env.TARGET_BOTS.split(',').map(s=
 OWNER_IDS: process.env.OWNER_IDS ? process.env.OWNER_IDS.replace(/['"]/g, '').split(',').map(id=>id.trim()).filter(Boolean) : [],
 BOT_USERNAME: process.env.BOT_USERNAME ? process.env.BOT_USERNAME.replace(/['"]/g, '').trim() : '',
 PUBLIC_URL: process.env.PUBLIC_URL ? process.env.PUBLIC_URL.replace(/['"]/g, '').trim() : '',
-FORCE_JOIN: [
-{ id: '@infowalzy', title: 'INFO WALZY', url: 'https://t.me/infowalzy' },
-{ id: '@historyfixred', title: 'HISTORY FIXRED', url: 'https://t.me/historyfixred' }
+FORCE_JOIN:[
+{id:'@infowalzy',title:'INFO WALZY',url:'https://t.me/infowalzy'},
+{id:'@historyfixred',title:'HISTORY FIXRED',url:'https://t.me/historyfixred'}
 ],
 DANA_NUMBER: process.env.DANA_NUMBER ? process.env.DANA_NUMBER.replace(/['"]/g, '').trim() : '083124469855',
 DANA_NAME: process.env.DANA_NAME ? process.env.DANA_NAME.replace(/['"]/g, '').trim() : 'WALZY STORE'
